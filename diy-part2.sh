b54be99972c5bc531970b45c532b40b5265a9829
segadrive <47469912+segadrive@users.noreply.github.com>
2026-02-14 19:26:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,18 +19,3 @@ sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generat
 # Modify hostname
 #sed -i 's/OpenWrt/360T7/g' package/base-files/files/bin/config_generate
 #!/bin/bash
-cd openwrt
-
-# 启用 BBRv3 核心配置（6.1内核原生支持）
-./scripts/config -e CONFIG_KERNEL_TCP_CONG_ADVANCED
-./scripts/config -e CONFIG_KERNEL_TCP_CONG_BBR3
-# 设置 BBRv3 为默认拥塞控制算法
-./scripts/config --set-str CONFIG_KERNEL_DEFAULT_TCP_CONG "bbr3"
-
-# 优化 360T7 性能（可选）
-./scripts/config -e CONFIG_KERNEL_CPU_FREQ_GOV_PERFORMANCE
-./scripts/config -e CONFIG_KERNEL_NET_SCH_CAQ
-./scripts/config -e CONFIG_KERNEL_NET_SCH_HTB
-
-# 更新配置
-make defconfig
