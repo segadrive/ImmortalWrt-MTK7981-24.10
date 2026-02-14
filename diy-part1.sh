18bee1667ae1b01b7d307e049503ccee34c5a271
segadrive <47469912+segadrive@users.noreply.github.com>
2026-02-14 19:26:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,16 +12,7 @@
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#!/bin/bash
-cd openwrt
 
-# 添加常用插件源（解决cloudflared/appfilter等插件依赖）
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-
-# 更新feeds（可选，提前拉取插件）
-./scripts/feeds update -a
-exit 0
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
